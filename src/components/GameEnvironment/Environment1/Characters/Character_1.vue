a942b4bf6d4a2f7a8b116e1566226e351cb773ad
F R Summit <www.frsummit1@gmail.com>
2020-03-16 21:00:53 +0600

    key code track added

--- src/components/GameEnvironment/Environment1/Characters/Character_1.vue
+++ src/components/GameEnvironment/Environment1/Characters/Character_1.vue
@@ -10,18 +10,28 @@ import $ from 'jquery'
 export default {
     created() {
         console.log('working')
+        let char1 = $('.char1').position()
+        console.log(char1)
+    },
+    mounted() {
+        window.addEventListener("keypress", function(e) {
+            console.log(String.fromCharCode(e.keyCode));
+        }.bind(this));
     },
     methods: {
         goRight() {
+            console.log('click')
             // $(document).keypress(function (eventObject) {
                 // console.log('eventObject.keyCode');
             // });
             // let top = document.querySelector('char1').offsetTop
             // console.log(top)
             // $('char1').css()
-            let top = $('.char1').position()
-            console.log(top)
+            let char1 = parseInt(document.querySelector('.char1').style.bottom)
+            // document.querySelector('.char1').style.left = char1 + 'px';
+            console.log(char1)
         }
+        
     }
 }
 </script>
