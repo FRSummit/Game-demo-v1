f198ca57f97c79abaa6905a2988b19eec54a972c
unknown <rimishehrin143@gmail.com>
2020-03-17 20:15:40 +0600

    created highest score

--- src/views/Highest_score/HighestScore.vue
+++ src/views/Highest_score/HighestScore.vue
@@ -1,5 +1,36 @@
 <template>
-    <div>
-        <h1>Hello</h1>
+    <div class="main-section">
+        <div class="score-section">
+            <table>
+                <tr>
+                    <th>Position</th>
+                    <th>Name</th>
+                    <th>Score</th>
+                </tr>
+                
+                <tr>
+                    <th>First</th>
+                    <th>Some name</th>
+                    <th>1000</th>
+                </tr>
+                
+                <tr>
+                    <th>Second</th>
+                    <th>No Name</th>
+                    <th>100</th>
+                </tr>
+            </table> 
+        </div>
+             
     </div>
 </template>
+
+<style scoped>
+ .score-section{
+     width: 50%;
+    margin: 0 auto;
+ }
+ .table{
+     width: 100%;
+ }
+</style>
